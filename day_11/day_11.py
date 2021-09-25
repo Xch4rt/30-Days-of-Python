76ec39c62d00919004c075a2a453add45e02e9af
Xch4rt <pab203.guti@gmail.com>
2021-09-24 20:54:41 -0600

    exercise 3 and 4

--- day_11/day_11.py
+++ day_11/day_11.py
@@ -20,4 +20,6 @@ add_all_nums(1,2,3,4,4,5)
 
 #Temperature in °C can be converted to °F using this formula: °F = (°C x 9/5) + 32. Write a function which converts °C to °F, convert_celsius_to-fahrenheit.
 
-
+def convert_celsius_to_fahrenheit(c):
+    return c * (9/5) + 32
+print(convert_celsius_to_fahrenheit(23))
