f7a35ff1c6feb2d8f0fd234d7eb04bcccd578142
Xch4rt <pab203.guti@gmail.com>
2021-10-05 02:07:23 -0600

    Day 16 of 30

--- day_16/day_16.py
+++ day_16/day_16.py
@@ -1,5 +1,5 @@
 #Get the current day, month, year, hour, minute and timestamp from datetime module
-from datetime import datetime
+from datetime import datetime, time, date
 
 now = datetime.now()
 time = now.strftime("%d/%m/%Y, %H:%M:%S")
@@ -10,18 +10,13 @@ print(time)
 time1 = now.strftime("%m/%d/%Y, %H:%M:%S")
 print(time1)
 #Today is 5 December, 2019. Change this time string to time.
-
+dat = "5 December, 2019"
+print(datetime.strptime(dat, "%d %B, %Y"))
 #Calculate the time difference between now and new year.
-
-
+dat1 = date(year = 2021,month=10,day=5)
+dat2 = date(year = 2018,month=2,day=20)
+print(dat1 - dat2)
 #Calculate the time difference between 1 January 1970 and now.
-
-
-'''
-Think, what can you use the datetime module for? Examples:
-
-    Time series analysis
-    To get a timestamp of any activities in an application
-    Adding posts on a blog
-
-'''
+datJ = date(year=1970, month = 1, day=1)
+datNow = date(year=2021, month=10, day=5)
+print(datNow - datJ)
