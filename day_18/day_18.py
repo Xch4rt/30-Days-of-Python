7a043da702adffed990fd48ebdce467054d11f79
Xch4rt <pab203.guti@gmail.com>
2021-10-08 11:32:08 -0600

    Ejercicio 2

--- day_18/day_18.py
+++ day_18/day_18.py
@@ -18,5 +18,6 @@ distance = 8 -(-4) # 12
 #Clean the following text. After cleaning, count three most frequent words in the string.
 sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
 
-matches = re.sub('','',sentence)
-#print(matches)
+matches = re.sub(r'[$%@&?!]','',sentence)
+
+print(matches)
