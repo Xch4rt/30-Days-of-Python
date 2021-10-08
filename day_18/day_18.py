b48a601fb29a134365eeb841baaf1ea8975dd3b0
Xch4rt <pab203.guti@gmail.com>
2021-10-08 11:17:26 -0600

    Empezando dia 18 de 30

---
+++ day_18/day_18.py
@@ -0,0 +1,22 @@
+import re
+from collections import Counter
+#What is the most frequent word in the following paragraph?
+
+paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
+words = re.findall(r'\w+',paragraph)
+cap_words = [word.upper() for word in words]
+word_count = Counter(cap_words)
+print(word_count)
+#The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers from this whole text and find the distance between the two furthest particles.
+
+points = ['-1', '2', '-4', '-3', '-1', '0', '4', '8']
+sorted_points =  [-4, -3, -1, -1, 0, 2, 4, 8]
+distance = 8 -(-4) # 12
+
+
+
+#Clean the following text. After cleaning, count three most frequent words in the string.
+sentence = '''%I $am@% a %tea@cher%, &and& I lo%#ve %tea@ching%;. There $is nothing; &as& mo@re rewarding as educa@ting &and& @emp%o@wering peo@ple. ;I found tea@ching m%o@re interesting tha@n any other %jo@bs. %Do@es thi%s mo@tivate yo@u to be a tea@cher!?'''
+
+matches = re.sub('','',sentence)
+#print(matches)
